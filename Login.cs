0c646e243876c6616de92dfe0feb3338bb1cc7f3
Hugo Henrique <hugohenriquelarah@gmail.com>
2025-03-05 20:28:33 -0300

    First poull test

---
+++ Login.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace SmartFrete
+{
+    internal class Login
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); 
+
+            Console.WriteLine("That's crazy my Bro!"); 
+
+        }
+    }
+}
